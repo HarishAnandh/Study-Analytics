f327ae29ad242bb879f05843b87020a5c5d41acf
Harish Anandh <harish.anandh@gmail.com>
2026-06-24 17:31:40 +0530

    Connected FastAPI to Supabase

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,65 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+
+from database import SessionLocal, engine, Base
+from models import Subject
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
     return {"message": "Study Analytics API Running"}
+
+@app.get("/subjects")
+def get_subjects():
+
+    db: Session = SessionLocal()
+
+    subjects = db.query(Subject).all()
+
+    result = []
+
+    for s in subjects:
+        result.append({
+            "id": s.id,
+            "name": s.name,
+            "difficulty": s.difficulty,
+            "proficiency": s.proficiency,
+            "priority": s.priority
+        })
+
+    db.close()
+
+    return result
+
+
+@app.post("/subjects")
+def add_subject(subject: dict):
+
+    db: Session = SessionLocal()
+
+    new_subject = Subject(
+        name=subject["name"],
+        difficulty=int(subject["difficulty"]),
+        proficiency=int(subject["proficiency"]),
+        priority=int(subject["priority"])
+    )
+
+    db.add(new_subject)
+    db.commit()
+    db.refresh(new_subject)
+
+    db.close()
+
+    return {"message": "Subject Added"}
---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Subject(Base):
+    __tablename__ = "subjects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    difficulty = Column(Integer)
+    proficiency = Column(Integer)
+    priority = Column(Integer)
